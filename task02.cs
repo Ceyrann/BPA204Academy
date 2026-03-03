8a86cc492bc8b82c50a10d87db892571ff412a7c
Ceyrann <ceiran006@gmail.com>
2026-03-03 17:01:47 +0400

    Create task02.cs
    task2

---
+++ task02.cs
@@ -0,0 +1,14 @@
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = 5;
+
+
+            Console.WriteLine($"{a + b}, { a - b }, {a * b}, { a / b},{a % b}");
+        }
+    }
+}
